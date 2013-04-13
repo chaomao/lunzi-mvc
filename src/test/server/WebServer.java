bbad35a467d2cb270e6e04b12749b5a224886089
Mao Chao <6233843@gmail.com>
2013-04-13 09:12:14 +0800

    [MC] add servlet test

--- src/test/server/WebServer.java
+++ src/test/server/WebServer.java
@@ -1,28 +1,15 @@
 package server;
 
-import org.mortbay.jetty.Handler;
-import org.mortbay.jetty.Request;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.handler.AbstractHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.servlet.ServletHolder;
 
 public class WebServer {
     public static void main(String[] args) throws Exception {
-        Handler handler = new AbstractHandler() {
-            public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch)
-                    throws IOException, ServletException {
-                response.setContentType("text/html");
-                response.setStatus(HttpServletResponse.SC_OK);
-                response.getWriter().println("<h1>Hello</h1>");
-                ((Request) request).setHandled(true);
-            }
-        };
         Server server = new Server(8080);
-        server.setHandler(handler);
+        Context context = new Context(server, "/");
+        context.addServlet(new ServletHolder(new HelloServlet()), "/*");
         server.start();
     }
 }
+
