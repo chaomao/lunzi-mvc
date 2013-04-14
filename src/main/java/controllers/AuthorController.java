58bbe33140f6e278e0a02a899332c1f6dc0884c2
Mao Chao <6233843@gmail.com>
2013-04-14 22:02:58 +0800

    [MC] add list model functionality

--- src/main/java/controllers/AuthorController.java
+++ src/main/java/controllers/AuthorController.java
@@ -1,15 +1,25 @@
 package controllers;
 
+import com.google.common.collect.Lists;
 import com.thoughtworks.mvc.annotations.Path;
 import com.thoughtworks.mvc.model.ModelMap;
 import models.Author;
 
 public class AuthorController {
 
-    @Path("/author/list")
-    public ModelMap list() {
+    @Path("/author/sample")
+    public ModelMap sample() {
         ModelMap modelMap = new ModelMap();
         modelMap.put("author", new Author("Mao Chao"));
         return modelMap;
     }
+
+    @Path("/author/list")
+    public ModelMap list() {
+        ModelMap modelMap = new ModelMap();
+        modelMap.put("authors", Lists.newArrayList(new Author("Mao Chao"), new Author("Wang Xiaofeng")));
+        return modelMap;
+    }
+
+
 }
