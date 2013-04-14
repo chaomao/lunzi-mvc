ba67b38acc5ea78ba49a0aabc075e555cc639080
Mao Chao <6233843@gmail.com>
2013-04-14 16:08:25 +0800

    [MC] add post form

--- src/main/java/com/thoughtworks/mvc/action/ActionCaller.java
+++ src/main/java/com/thoughtworks/mvc/action/ActionCaller.java
@@ -1,13 +1,17 @@
 package com.thoughtworks.mvc.action;
 
 import com.thoughtworks.mvc.IOCContainer;
+import com.thoughtworks.mvc.annotations.RequestParameter;
 import com.thoughtworks.mvc.http.HttpMethodsType;
 import com.thoughtworks.mvc.model.ModelAndView;
 import com.thoughtworks.mvc.model.ModelMap;
 import com.thoughtworks.mvc.view.resolver.ViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class ActionCaller {
     private final ActionDefinition definition;
@@ -25,13 +29,42 @@ public class ActionCaller {
     public ModelAndView run(HttpServletRequest request) {
         try {
             Object controller = IOCContainer.getInstance().get(definition.controller);
-            Object result = definition.action.invoke(controller);
-            ModelMap modelMap = new ModelMap();
-            modelMap.put(definition.viewModelName, result);
+            Object result = definition.action.invoke(controller, getParameters(request));
+            ModelMap modelMap = createModelMap(result);
             return new ModelAndView(modelMap, getViewPath());
         } catch (InvocationTargetException | IllegalAccessException e) {
             throw new RuntimeException();
         }
+    }
+
+    private Object[] getParameters(HttpServletRequest request) {
+        ArrayList<Object> objects = new ArrayList<>();
+        Annotation[][] parameterAnnotations = definition.action.getParameterAnnotations();
+        Class<?>[] parameterTypes = definition.action.getParameterTypes();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            Annotation annotation = parameterAnnotations[i][0];
+            String paraName = getParameterName(annotation, parameterTypes[i]);
+            objects.add(request.getParameter(paraName));
+        }
+        return objects.toArray();
+    }
+
+    private String getParameterName(Annotation annotation, Class<?> parameterType) {
+        if (annotation instanceof RequestParameter) {
+            return ((RequestParameter) annotation).value();
+        } else {
+            return parameterType.getName();
+        }
+    }
+
+    private ModelMap createModelMap(Object result) {
+        ModelMap modelMap = new ModelMap();
+        if (result instanceof String) {
+            modelMap.put(definition.viewModelName, result);
+        } else if (result instanceof ModelMap) {
+            modelMap.putAll((Map<? extends String, ?>) result);
+        }
+        return modelMap;
     }
 
     private String getViewPath() {
--- src/main/java/controllers/PostController.java
+++ src/main/java/controllers/PostController.java
@@ -2,13 +2,21 @@ package controllers;
 
 import com.thoughtworks.mvc.annotations.Path;
 import com.thoughtworks.mvc.annotations.Post;
-import com.thoughtworks.mvc.annotations.ViewModel;
+import com.thoughtworks.mvc.annotations.RequestParameter;
+import com.thoughtworks.mvc.model.ModelMap;
 
 public class PostController {
-    @Path("/submit")
+
+    @Path("/post/new")
+    public void newForm() {
+    }
+
+    @Path("/post/submit")
     @Post
-    @ViewModel(name = "content")
-    public String submit() {
-        return "You have submit successful";
+    public ModelMap submit(@RequestParameter("name") String name) {
+        ModelMap modelMap = new ModelMap();
+        System.out.println(name);
+        modelMap.put("name", name);
+        return modelMap;
     }
 }
